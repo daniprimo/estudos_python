53a769246005bc71e2e67e9a4a45267c21de7144
DLLM <DLLM@gft.com>
2023-02-26 14:20:15 -0300

    feat: buscando os dados do banco de dados postgrae

--- galeria/views.py
+++ galeria/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
+from galeria.models import Fotografia
  
 
 def index(request):
@@ -14,8 +15,10 @@ def index(request):
             "legenda": "nasas.org / NASA / Neymar "}
   
     }
-    return render(request, 'galeria/index.html', {"cards": dados})
+    fotografias = Fotografia.objects.all()
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html', {"cards": dados})
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
 
