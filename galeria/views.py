d2c3971bc639786def612866815382c7e76c13d2
DLLM <DLLM@gft.com>
2023-02-25 23:24:40 -0300

    Realizando a migracao de dados

--- galeria/views.py
+++ galeria/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 
+ 
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    dados = {
+        1: {"nome": "nebulosa de carina",
+            "legenda": "webbtelecope.org / NASA /  Webb"},
+        2: {"nome": "Galáxia ngc 1079",
+            "legenda": "nasa.org / NASA / Neymar "},
+        3: {"nome": "nebussosa de carina",
+            "legenda": "websbtelecope.org / NASA /  Webb"},
+        4: {"nome": "Galássxia ngc 1079",
+            "legenda": "nasas.org / NASA / Neymar "}
+  
+    }
+    return render(request, 'galeria/index.html', {"cards": dados})
 
 def imagem(request):
-    return render(request, 'galeria/imagem.html')
+    return render(request, 'galeria/imagem.html', {"cards": dados})
 
