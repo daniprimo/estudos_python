a55590d2dd7ad388161958acb7a8ac1d9074efda
DLLM <DLLM@gft.com>
2023-03-02 10:55:58 -0300

    feat: finalizando estudos basicos com o  delete

--- clientes/views.py
+++ clientes/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Person
 from .forms import PersonForm
 def person_list(request):
@@ -7,9 +7,28 @@ def person_list(request):
 
 
 def person_new(request) :
-    form = PersonForm(request.POST, request.FILES,None)
+    form = PersonForm(request.POST, request.FILES or None)
     if form.is_valid():
         form.save()
         return redirect('person_list')
     return render(request, 'person_form.html', {'form': form})
 
+def person_update(request, id):
+    person = get_object_or_404(Person, pk=id)
+    form = PersonForm(request.POST or None, request.FILES or None, instance=person)
+  
+    if form.is_valid():
+        form.save()
+        return redirect('person_list')
+
+    return render(request, 'person_form.html', {'form': form})
+
+
+def person_delete (request, id):
+    person = get_object_or_404(Person, pk=id)
+    form = PersonForm(request.POST or None, request.FILES or None, instance=person)
+    if request.method == 'POST':
+        person.delete()
+        return redirect('person_list')
+    
+    return render(request, 'person_delete_confirm.html', {'person': person})
