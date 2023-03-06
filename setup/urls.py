8c938e2ff33dfba622d00a6e5b114a97481cc4a4
DLLM <DLLM@gft.com>
2023-03-06 10:38:30 -0300

    feat: atualizando

--- setup/urls.py
+++ setup/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     path('articles/<int:years>/', articles),
     path('person/', include(clientes_urls)),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
